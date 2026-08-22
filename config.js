ddedf35e77e144a24ca2318c7dfc20af8529ec05
retoziegler <56159492+retoziegler@users.noreply.github.com>
2026-08-22 07:30:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Hier deine Google Apps Script Web-App URL eintragen:
-const API_URL = "https://script.google.com/macros/s/DEINE_AKTUELLE_APPS_SCRIPT_ID/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbz847hIZo1s-NcPHEn4TceW2UuABSz69bILVIp7OvyN9__bBg4ewZOnpAspQLEvTts/exec";
