6d6ed8903f5b3b30227dd1e616f8bae6a8a46bdd
retoziegler <56159492+retoziegler@users.noreply.github.com>
2026-08-22 07:44:57 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'kaffee-qr-v4';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './config.js',
+  'https://cdn.jsdelivr.net/npm/jsqr@1.4.0/dist/jsQR.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
